3b5e91674fbc1160948a41b834b33589a0414f59
FireLord1 <firelord.xda@gmail.com>
2016-09-24 02:10:47 +0530

    p1a42: Add basic setup
    -FileSystem stuff
    -Model number things
    -Bluetooth headers
    
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- full_p1a42.mk
+++ full_p1a42.mk
@@ -1,30 +1,29 @@
-# Copyright (C) 2016 The CyanogenMod Project
+#
+# Copyright (C) 2015 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-$(call inherit-product, device/lenovo/p1a42/full_p1a42.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Release name
-PRODUCT_RELEASE_NAME := p1a42
+# Inherit from passion row device
+$(call inherit-product, device/lenovo/p1a42/device.mk)
 
-# Boot animation
-TARGET_SCREEN_WIDTH := 1080
-TARGET_SCREEN_HEIGHT := 1920
-
-## Device identifier. This must come after all inclusions
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := p1a42
-PRODUCT_NAME := cm_p1a42
+PRODUCT_NAME := full_p1a42
 PRODUCT_BRAND := Lenovo
-PRODUCT_MANUFACTURER := Lenovo
-PRODUCT_RELEASE_NAME := p1a42
 PRODUCT_MODEL := Vibe P1
+PRODUCT_MANUFACTURER := lenovo
